a1a2013594a9450cb13f164f78fac58c246dbf02
David Parlevliet <dave@leaguecraft.com>
2015-06-17 16:25:16 +1000

    bah

--- proxy.php
+++ proxy.php
@@ -107,7 +107,7 @@ if (isset($json['queries']) && is_array($json['queries']))
       {
         $results[$offset] = $returned_json[0];
       } else {
-        echo $returned_json;
+        var_dump($returned_json);
       }
     } else {
       $results[$offset] = Array(
