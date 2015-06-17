8953fb9a43a36ebb5b911a75bd1146290e5d816d
David Parlevliet <dave@leaguecraft.com>
2015-06-17 11:29:01 +1000

    bah

--- proxy.php
+++ proxy.php
@@ -42,7 +42,6 @@ if (sizeof($json) == 0)
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
       header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    exit;
   } else {
     // really?
     http_response_code(412);
